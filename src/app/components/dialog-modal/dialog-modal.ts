e015be9af85358b9d0f342c8828240037df7cd6b
Petro <petrotymchuck@gmail.com>
2025-09-01 07:53:26 +0300

    add modal component

---
+++ src/app/components/dialog-modal/dialog-modal.ts
@@ -0,0 +1,30 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {NgClass, NgIf, NgStyle} from '@angular/common';
+
+@Component({
+  selector: 'app-dialog-modal',
+  imports: [
+    NgClass,
+    NgStyle,
+    NgIf
+  ],
+  templateUrl: './dialog-modal.html',
+  styleUrl: './dialog-modal.css'
+})
+export class DialogModal {
+  @Input() title = 'Модалка';
+  @Input() isOpen = false;
+
+  @Output() closed = new EventEmitter<void>();
+  @Output() submitted = new EventEmitter<void>();
+
+  close() {
+    console.log("close")
+    this.closed.emit();
+  }
+
+  submit() {
+    this.submitted.emit();
+    this.close();
+  }
+}
