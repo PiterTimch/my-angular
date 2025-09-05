b720d18244756fbc0d60c2a635327a6b32ab3387
Petro <petrotymchuck@gmail.com>
2025-09-05 08:29:30 +0300

    add base register form

--- src/app/pages/auth/register/register.ts
+++ src/app/pages/auth/register/register.ts
@@ -1,11 +1,71 @@
 import { Component } from '@angular/core';
+import {AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors} from '@angular/forms';
+import {CategoryService} from '../../../services/category.service';
+import {AccountService} from '../../../services/account.service';
+import {serialize} from 'object-to-formdata';
+import {ErrorUtils} from '../../../utils/ErrorUtils';
+import {Router} from '@angular/router';
+import {KeyValuePipe, NgForOf, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    NgIf,
+    NgForOf,
+    KeyValuePipe
+  ],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
 export class Register {
+  imagePreview: string | ArrayBuffer | null = null;
+  registerForm: FormGroup;
+  errorMessage: string | null = null;
 
+  requiredMessage(message: string) {
+    return (control: AbstractControl): ValidationErrors | null => {
+      return !control.value ? { message } : null;
+    };
+  }
+
+  constructor(private fb: FormBuilder, private accountService: AccountService, private router: Router) {
+    this.registerForm = this.fb.group({
+      firstName: ['', this.requiredMessage('Ім\'я є обов\'язковим')],
+      lastName: ['', this.requiredMessage('Прізвище є обов\'язковим')],
+      email: ['', this.requiredMessage('Пошта є обов\'язковою')],
+      password: ['', this.requiredMessage('Пароль є обов\'язковим')],
+      imageFile: null
+    });
+  }
+
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      if (!file.type.startsWith('image/')) {
+        this.errorMessage = 'Можна завантажувати лише зображення';
+        return;
+      }
+      this.registerForm.patchValue({ imageFile: file });
+      this.imagePreview = URL.createObjectURL(file);
+    } else {
+      this.registerForm.patchValue({ imageFile: null });
+      this.imagePreview = null;
+    }
+  }
+
+  onSubmit() {
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    const formData = serialize(this.registerForm.value);
+
+    this.accountService.registerUser(formData).subscribe({
+      next: () => this.router.navigate(['/']),
+      error: (err) => {
+        this.errorMessage = ErrorUtils.handleValidation(err, this.registerForm);
+      }
+    });
+  }
 }
--- src/app/services/account.service.ts
+++ src/app/services/account.service.ts
@@ -1,19 +1,16 @@
 import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
-import {Observable} from 'rxjs';
-import {ICategory, ICategoryCreate} from '../models/Category';
 import { environment } from '../../environments/environment';
-import {IRegister} from '../models/Account';
 
 @Injectable({
   providedIn: 'root'
 })
 
-export class CategoryService {
+export class AccountService {
   private apiURL = environment.apiURL + "account/";
   constructor(private http: HttpClient) {}
 
-  registerUser(model : IRegister) {
-    this.http.post(this.apiURL + "register", model);
+  registerUser(formData: FormData) {
+    return this.http.post(this.apiURL + "register", formData);
   }
 }
